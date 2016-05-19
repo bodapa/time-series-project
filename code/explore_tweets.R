e9ec8051f405c53fca60441e98e281eb209ab21a
dyuval <yuvaldagan4@gmail.com>
2016-05-19 01:07:27 -0500

    changed data based on changes to python parser chunck-1 seems to be formatted okay

--- code/explore_tweets.R
+++ code/explore_tweets.R
@@ -1,9 +1,10 @@
 
-# loading chuncks 0-8 to one data frame:
-
+# loading chuncks 0 to a data frame:
 tweets <- readRDS(file = "..//data//chunck_0.rds")
-for (i in 1:8){
-        print(i)
+
+# if we want to load more than one...
+for (i in 0:8){
+        #print(i)
         file_name = paste("..//data//chunck_" , i, ".rds", sep = "")
         temp <- readRDS(file = file_name)
         tweets <- rbind(tweets , temp)
@@ -12,5 +13,31 @@ for (i in 1:8){
 
 dim(tweets)
 summary(tweets)
-# need some cleaning...
+names(tweets)
+str(tweets) #?
+head(tweets)
+
+# number of users in this data set:
+length(unique(tweets$user_id))
+# type is = status. with is good
+unique(tweets$type)
+table(tweets$type)
+
+# tweet_id is numeric:
+summary(tweets$tweet_id)
+
+# re-tweet count is also numeric
 summary(tweets$ret_count)
+
+# favorite has only one value (in the frst chunck) = false
+summary(tweets$favorite)
+
+# looking at the top 10 Hashtags:
+sort(table(tweets$hashtags),decreasing = TRUE)[1:10]
+# GoodMorning is more popular than iPad :)
+
+# need to convert column 'time' to date-time format:
+
+library(lubridate)
+parse_date_time((tweets$time)[1], orders = "a% b% d!% H!%:M!%:S!% y!")
+
